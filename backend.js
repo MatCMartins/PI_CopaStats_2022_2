9baac564e1a7bc9e45881e71be3703ec998d4de0
MateusCMartins724 <mateus.c.martins@outlook.com>
2022-11-08 21:24:44 -0300

    Termino da página de jogadores (versao manual)

---
+++ backend.js
@@ -0,0 +1,7 @@
+function ShowDiv() {
+    document.getElementById("jogadores1").style.display = "block";
+    document.getElementById("jogadores2").style.display = "block";
+    document.getElementById("jogadores3").style.display = "block";
+    document.getElementById("jogadores4").style.display = "block";
+    document.getElementById("jogadores5").style.display = "block";
+}
